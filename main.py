503a194829a6fc26d8fb5f32b658132529249ad6
TheRoundEyes <mj.reyes@nzme.co.nz>
2023-06-29 17:55:08 +1200

    Test

---
+++ main.py
@@ -0,0 +1,29 @@
+user_prompt = "Enter a Task: "
+
+todo_list = []
+while True:
+    user_action = input("Type add or show: ")
+
+    match user_action:
+        case 'add':
+                todo = input("Enter a task: ")
+                todo_list.append(todo.capitalize())
+        # | <---- bitwise or Operator
+        case 'show' | 'display':
+                for task in todo_list:
+                      print(task)
+        case 'close':
+                exit()
+        #when entering gibberish
+        case _:
+                print("Wrong command")
+
+
+
+
+
+
+
+
+
+## For-loops & Match-case
